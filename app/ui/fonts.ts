182750a93fa3ddc4a32d32582b4ddc9d9ee8dbdc
Manuel Lopez Anido <manuellopezanido@gmail.com>
2024-03-10 16:52:37 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Actor, Lusitana } from 'next/font/google';
+ 
+export const interFont = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana ({subsets: ["latin"], weight: ["400" , "700"]})
+//export const lusitanaFont = Actor ({subsets: ["latin"], weight: ["400" ]})
